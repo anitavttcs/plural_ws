4181ec8136001a7818ec7b16e1a7f19adaff36b7
anitavttcs <anita.vertetics@hotmail.com>
2020-12-09 15:08:21 +0100

    commented version

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+// létrehozzuk a _load függvényt 
 function _load(){
   
+  // változóba mentjük a root div elemet
   let rootE = document.getElementById("root");
 
+  // létrehozunk egy stringekből álló tömböt
   let aTextArray = ["alma", "cseresznye", "eper", "dinnye", "szeder", "mandarin", "ribizli", "narancs", "paradicsom", "citrom", "banana", "datolya", "szilva", "barack", "pomelo", "kiwi","grapefruit", "kaki"];
 
   //console.log(aTextArray.length);
 
+  // a tömb elemeiből fruit class-ú div elemeket hozunk létre for ciklussal
   for (let index = 0; index < aTextArray.length; index++) {
 
     //console.log(aTextArray[index]);
@@ -17,21 +21,29 @@ function _load(){
   }
   //console.log(document.getElementsByClassName("fruit"));
 
+  // létrehozunk egy fruitDivs változót, amiben eltároljuk az összes fruit class-ú elemet
   let fruitDivs = rootE.querySelectorAll(".fruit");
+  
+  // kiíratjuk a tömb eleminek számát
   console.log(fruitDivs.length);
 
+  // for ciklussal végigmegyünk a tömb elemein
   for (let index = 0; index < fruitDivs.length; index++) {
     //console.log(fruitDivs[index]);
     //fruitDivs[index].classList.add("anotherClass");
-
+    
     //console.log(index === 1);
     //console.log(index % 2 === 0);
+
+    // minden páros indexű elemre beállítjuk a trueClass-t
     if(index % 2 === 0){
       fruitDivs[index].classList.add("trueClass");
-
+      
+    // ha az elem indexe páratlan és maradék nélkül osztható 5-tel, akkor az anotherClass-t
     } else if(index % 5 === 0) {
       fruitDivs[index].classList.add("anotherClass");
-
+      
+    // az 5-tel maradék nélkül nem osztható páratlan elemekre a falseClass-t
     } else {
       fruitDivs[index].classList.add("falseClass");
     }
@@ -40,5 +52,5 @@ function _load(){
 
 
 }
-
+// hozzárendeljük a window objektumhoz a _load függvényt a load eseményre
 window.addEventListener("load", _load);
